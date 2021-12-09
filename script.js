3b119979081db61953196a91877ceb2e2b91f135
yadavraj432 <95872375+yadavraj432@users.noreply.github.com>
2021-12-09 20:16:12 +0400

    Slider

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-function openmenu() {
-  document.getElementById("second").classList.add("second");
-  document.getElementById("second").style.display = "block";
-  document.getElementById("second").style.opacity = "1";
-  document.getElementById("main").style.opacity = "0.3";
 
-}
 
-function closeMenu() {
-  document.getElementById("second").style.display = "none";
-  document.getElementById("main").style.opacity = "1";
 
-  document.getElementsByClassName
-}
+
+
+
+
+
+
 
 
